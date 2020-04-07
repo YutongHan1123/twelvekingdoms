c9fcbdb8e2643c36f5beb1e7fb4d2a9c27ede3bd
YutongHan1123 <42613354+YutongHan1123@users.noreply.github.com>
2020-04-07 17:16:54 -0400

    Update bullet.js

--- assets/js/bullet.js
+++ assets/js/bullet.js
@@ -1,4 +1,4 @@
-var fileName = "../assets/data/bullet.csv";
+var fileName = "assets/data/bullet.csv";
 var bulletFields = ["1", "2","3", "4", "5", "6", "7", "8","9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"];
 
 d3.csv(fileName, function(error, data) {
